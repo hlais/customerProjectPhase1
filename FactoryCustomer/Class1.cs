d430bc29b554baf75e8b750276aa939bcbe48ac6
Halim Lais <halimlais@hotmail.com>
2018-02-28 18:05:59 +0000

    Initial commit

---
+++ FactoryCustomer/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MiddleLayer;
+
+namespace FactoryCustomer
+{
+    public  static class Factory // design patten (SIMPLE FACTORY PATTERN)
+    {
+        private static Dictionary<string, CustomerBase> _custs = new Dictionary<string, CustomerBase>();
+
+        //will remove contructor since its loading libraries which we may not want to instiate both libraries
+        //  static Factory() 
+        // {
+        //whether we want these libraries or not they are still loaded
+        //   _custs.Add("Customer", new Customer());   /// move to CustomerBase
+        //    _custs.Add("Lead", new Lead());           /// move to CustomerBase
+        // }
+        public static CustomerBase Create (string TypeOfCustomer)
+        {
+            //design pattern (LAZY LOADING) - Load objects on demand
+            //if custs is called then the cust librarys are loaded. else not. since 0
+            if (_custs.Count == 0)
+            {
+                _custs.Add("Customer", new Customer());
+                _custs.Add("Lead", new Lead());
+            }
+            //disign pattern (RIP - Replace IF with Polymophism )
+            return _custs[TypeOfCustomer];
+        }
+    }
+}
---
+++ WinformCustomer/FormCustomer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MiddleLayer;
+using FactoryCustomer;
+
+
+namespace WinformCustomer
+{
+    public partial class FormCustomer : Form
+    {
+        private CustomerBase _customer = null;
+       
+        public FormCustomer()
+        {
+            InitializeComponent();
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _customer = Factory.Create(comboBox1.Text);
+        }
+        private void SetCustomer()
+        {
+            _customer.CusterName = textBox1.Text;
+            _customer.PhoneNumber = textBox2.Text;
+            _customer.BillDate = Convert.ToDateTime(textBox4.Text);
+            _customer.BillAmount = Convert.ToDecimal(textBox3.Text);
+            _customer.Address = textBox5.Text;
+        }
+
+        private void FormCustomer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SetCustomer();
+                _customer.Validate();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
